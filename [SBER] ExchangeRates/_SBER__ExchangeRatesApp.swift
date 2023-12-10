42b6bd6e6f216c31eaa1f6c742144cb48323f175
Danila Goriachkin <dndv279@gmail.com>
2023-12-10 08:35:00 +0300

    Initial Commit

---
+++ [SBER] ExchangeRates/_SBER__ExchangeRatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  _SBER__ExchangeRatesApp.swift
+//  [SBER] ExchangeRates
+//
+//  Created by Danila on 10.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct _SBER__ExchangeRatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
